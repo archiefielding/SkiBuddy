5d274b4d41516e231016387822e228d534a68909
Aaron <aarongkendall@gmail.com>
2015-11-04 11:27:32 +0000

    header authentication testing

--- js/resortSearchController.js
+++ js/resortSearchController.js
@@ -1,3 +1,16 @@
-resortSearch.controller('resortSearchController', [function() {
+resortSearch.controller('resortSearchController', ['$http', function($http) {
+  var self = this;
 
+  self.searchResults = [];
+
+  self.doSearch = function(destination, startdate) {
+    $http({
+      method: 'GET',
+      url: 'https://api.qalendra.com/assets?date=' + startdate + '&country=' + destination,
+      headers: {'Authorization': 'Basic 9mpOb6J73PKLrw1Irax2i4pmXy112W6cj2Mo2qtm:'}
+    }).then(function successCallback(response) {
+      self.searchResults = response;
+      console.log(self.searchResults)
+    });
+  };
 }]);
