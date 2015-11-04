cfd983f1587487c6ac6b8fc450b31b8d82e67d4c
Aaron <aarongkendall@gmail.com>
2015-11-04 12:03:09 +0000

    header authentication testing

--- js/resortSearchController.js
+++ js/resortSearchController.js
@@ -3,11 +3,12 @@ resortSearch.controller('resortSearchController', ['$http', function($http) {
 
   self.searchResults = [];
 
+  $http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode('----myusername----' + ':' + '----mypass---');
   self.doSearch = function(destination, startdate) {
     $http({
       method: 'GET',
-      url: 'https://api.qalendra.com/assets?date=' + startdate + '&country=' + destination,
-      headers: {'Authorization': 'Basic 9mpOb6J73PKLrw1Irax2i4pmXy112W6cj2Mo2qtm:'}
+      url: 'https://api.qalendra.com/assets?date=' + startdate + '&countryCode=' + destination,
+      headers: { 'Authorization': 'Basic 9mpOb6J73PKLrw1Irax2i4pmXy112W6cj2Mo2qtm:' }
     }).then(function successCallback(response) {
       self.searchResults = response;
       console.log(self.searchResults)
