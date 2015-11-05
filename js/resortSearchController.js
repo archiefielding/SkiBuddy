514fc778742c241c457f828f98eaccdddd10c322
Aaron <aarongkendall@gmail.com>
2015-11-05 14:22:14 +0000

    ng-model not binding

--- js/resortSearchController.js
+++ js/resortSearchController.js
@@ -3,11 +3,11 @@ resortSearch.controller('resortSearchController', ['$http', function($http) {
 
   self.searchResults = [];
 
-  self.doSearch = function(destination, startdate) {
+  self.doSearch = function(destination, startdate, enddate) {
     $http({
       method: 'GET',
       url: 'https://gentle-reaches-8946.herokuapp.com/',
-      params: {date: startdate, countryCode: destination}
+      params: {date: startdate, countryCode: destination, endDate: enddate}
       }).then(function successCallback(response) {
         self.searchResults = response.data;
         console.log(self.searchResults)
