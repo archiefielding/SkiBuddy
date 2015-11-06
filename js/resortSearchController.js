8d48e49860ebec3630bcdc5485959363a4613f82
danielstpaul <danielstpaul@hotmail.com>
2015-11-06 14:39:47 +0000

    commit

--- js/resortSearchController.js
+++ js/resortSearchController.js
@@ -13,4 +13,25 @@ resortSearch.controller('resortSearchController', ['$http', function($http) {
         console.log(self.searchResults)
       });
     };
+
+    self.resort_ratings = [];
+
+    for (day_idx in self.searchResults) {
+      resorts = self.searchResults[day_idx]
+      console.log(resorts);
+      for (r_idx in resorts) {
+        aId = resorts[r_idx].assetId
+        if (!resort_ratings[aId]) {
+          resort_ratings[aId] = []
+        }
+        resort_ratings[aId].push(resorts[r_idx].predictions.data[0].score)
+      }
+    }
+
+    for (aId in resort_ratings) {
+      ratings_sum = self.resort_ratings[aId].reduce(function(a, b){return a+b;})
+      ratings_avg = ratings_sum / resort_ratings[aId].length
+      console.log("AssetID: " + aId + " average rating: " + ratings_avg)
+    }
+
 }]);
