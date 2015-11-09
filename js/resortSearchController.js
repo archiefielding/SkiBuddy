fe05a650b316296be61bb9ab03396b05075bba1c
Aaron <aarongkendall@gmail.com>
2015-11-09 09:30:59 +0000

    Removal of promise directive

--- js/resortSearchController.js
+++ js/resortSearchController.js
@@ -1,4 +1,4 @@
-resortSearch.controller('resortSearchController', ['$http', '$cookies', '$location', '$q', function($http, $cookies, $location, $q) {
+resortSearch.controller('resortSearchController', ['$http', '$cookies', '$location', function($http, $cookies, $location) {
   var self = this;
 
   self.setCookies = function(destination, startdate, enddate) {
